653599dd914bc82222aa2e953d3bb49a7560ddcd
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2024-10-29 17:55:15 -0500

    Update

--- lab4/zwatson-lab4.js
+++ lab4/zwatson-lab4.js
@@ -1,14 +1,22 @@
 (function(){
+let hero = document.getElementById('hero');
 document.getElementsByTagName('h1')[0].innerHTML = "Supercharge Your Brand with Stellar Marketing";
 document.querySelector('#hero > p').innerHTML= "Leverage innovative strategies from Stellar Marketing to make your business <b><i>shine</i></b> and <b><i>succeed.</i></b>";
-document.querySelector('#hero').style.background= "url(https://picsum.photos/id/683/1280/720)";
+hero.style.background= "url(https://picsum.photos/id/683/1280/720)";
 
 
-let header = document.getElementsByTagName('header')[0]
+let header = document.getElementsByTagName('header')[0];
 header.classList.add('bg-gray-800');
 //let footer = document.getElementById('footer')[0]
 
+document.querySelector('#hero > a').remove();
 
+
+const ctaSection = document.createElement('section');
+document.getElementsByTagName('main')[0].insertBefore(ctaSection, document.querySelector('#services'));
+
+
+const newCta = document.createElement('a');
 
 
 }

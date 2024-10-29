c64b0834cb300d052390da5a1fad8d79f5a1d270
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2024-10-29 16:53:02 -0500

    Update

--- lab4/zwatson-lab4.js
+++ lab4/zwatson-lab4.js
@@ -1,5 +1,15 @@
 (function(){
 document.getElementsByTagName('h1')[0].innerHTML = "Supercharge Your Brand with Stellar Marketing";
-}
+document.querySelector('#hero > p').innerHTML= "Leverage innovative strategies from Stellar Marketing to make your business <b><i>shine</i></b> and <b><i>succeed.</i></b>";
+document.querySelector('#hero').style.background= "url(https://picsum.photos/id/683/1280/720)";
 
+
+let header = document.getElementsByTagName('header')[0]
+header.classList.add('bg-gray-800');
+//let footer = document.getElementById('footer')[0]
+
+
+
+
+}
 )();
